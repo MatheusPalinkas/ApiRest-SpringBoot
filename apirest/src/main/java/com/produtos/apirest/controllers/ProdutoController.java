edc6c4c2ab95f2755e4f4e2ea876914a64d85d90
MatheusPalinkas <matheuspalinkas31@gmail.com>
2020-04-03 01:24:19 -0300

    Get e Post

--- apirest/src/main/java/com/produtos/apirest/controllers/ProdutoController.java
+++ apirest/src/main/java/com/produtos/apirest/controllers/ProdutoController.java
@@ -1,4 +1,30 @@
 package com.produtos.apirest.controllers;
 
+import com.produtos.apirest.models.Produto;
+import com.produtos.apirest.repository.ProdutosRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value="/api")
 public class ProdutoController {
+    @Autowired
+    ProdutosRepository produtosRepository;
+
+    @GetMapping("/produtos")
+    public List<Produto> show(){
+        return produtosRepository.findAll();
+    }
+
+    @GetMapping("/produto/{id}")
+    public Produto index(@PathVariable(value="id") long id){
+        return produtosRepository.findById(id);
+    }
+    @PostMapping("/produto")
+    public Produto create(@RequestBody Produto produto){
+        return produtosRepository.save(produto);
+    }
+
 }
